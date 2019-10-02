758c474f1f6157f5a765118e9dd5ef1553fdae24
ranganadh234 <ranganadh234@gmail.com>
2019-10-02 18:16:43 +0530

    Implemented the Pagination

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import login,logout,register,dashboard
 urlpatterns=[
             path('login/',login,name='login'),
-            path('login/',logout,name='logout'),
+            path('logout/',logout,name='logout'),
             path('register/',register,name='register'),
             path('dashboard',dashboard,name='dashboard')
 ]
--- search/views.py
+++ search/views.py
@@ -6,17 +6,17 @@ import requests
 
 def search_view(request):
     #city_id
-    if request.GET['city_id']:
-        print('inside city_id')
-        city_id=request.GET['city_id']
+    global queryset
+    if request.GET.get('city_id',False):
+        city_id=request.GET.get('city_id',False)
         if city_id:
             url="https://developers.zomato.com/api/v2.1/collections?city_id="+str(city_id)
             req=requests.get(url, headers = {"User-key": '1ec5908c1e671471d9bd924468eb41af'})
             queryset=req.json()
 
     #city_name
-    if  request.GET['city_name']:
-        city_name=request.GET['city_name']
+    if  request.GET.get('city_name',False):
+        city_name=request.GET.get('city_name',False)
         url="https://developers.zomato.com/api/v2.1/cities?q="+str(city_name)
         req=requests.get(url, headers = {"User-key": '1ec5908c1e671471d9bd924468eb41af'})
         city_id=req.json().get('location_suggestions').pop().get('id')#for getting first element we are using 0
@@ -28,7 +28,6 @@ def search_view(request):
     paginator=Paginator(queryset['collections'],6)
     page=request.GET.get('page')
     paged_list=paginator.get_page(page)
-    print('paged_list:',paged_list)
     context={
             "queryset":paged_list
     }
